7527ec4449952fad0e8b9e5c01746e2599686cad
Ahrend <caguilar@agrokasa.com.pe>
2024-06-22 10:52:45 -0500

    metodo async

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+Future<String> getTitle(){
+ return Future.delayed(Duration(seconds: 3),(){
+    return "Mi Planta de naranja Lima";
+  });
+}
+
+String title = "Sin titulo";
+
+setTitle()async{
+  title = await getTitle();
+  setState(() {});
+}
+
+@override
+  void initState() {
+   //getTitle().then((value){
+   //   title = value;
+   //   setState(() {});
+   // });
+    setTitle();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
